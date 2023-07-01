ad25e3efdba9c13c309e3b39be6160ded5545a1b
= <=>
2023-07-01 11:16:33 -0500

    First commit

---
+++ src/Input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner in = new Scanner(System.in);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int num : arr)
+        {
+            System.out.print(num + " ");
+        }
+    }
+}
